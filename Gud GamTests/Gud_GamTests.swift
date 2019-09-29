3c4990567d8e20472d127f40becc5c29fb30061c
Levi Nelson <levibb11@gmaill.com>
2019-09-29 13:42:24 -0700

    First App

---
+++ Gud GamTests/Gud_GamTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gud_GamTests.swift
+//  Gud GamTests
+//
+//  Created by Levi Nelson on 9/29/19.
+//  Copyright © 2019 Get Swifty. All rights reserved.
+//
+
+import XCTest
+@testable import Gud_Gam
+
+class Gud_GamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
